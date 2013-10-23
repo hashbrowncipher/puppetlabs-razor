959d03f1a00e378cd3ecabe50324c488923a99f4
fhats <fhats@yelp.com>
2013-10-22 20:36:03 -0700

    Use torquebox package instead of the stock install procedure

--- manifests/torquebox.pp
+++ manifests/torquebox.pp
@@ -1,26 +1,9 @@
 class razor::torquebox {
-  $url  = 'http://torquebox.org/release/org/torquebox/torquebox-dist/3.0.0/torquebox-dist-3.0.0-bin.zip'
-  $root = 'torquebox-3.0.0'
-  $dest = '/opt/razor-torquebox'
   $user = 'razor-server'
 
-  # Put the archive into place, if needed.
-  exec { "install torquebox binary distribution to ${dest}":
-    provider => shell,
-    command  => template('razor/install-zip.sh.erb'),
-    path     => '/bin:/usr/bin:/usr/local/bin:/opt/bin',
-    creates  => "${dest}/jruby/bin/torquebox",
-    require  => [Package[curl], Package[unzip]]
-  }
-
-  user { $user:
-    ensure   => present,
-    system   => true,           # system -- daemon -- user, please
-    password => '*',            # no password logins, please
-    home     => $dest,
-    shell    => '/bin/bash',    # if it comes up, let's be common
-    comment  => "razor-server daemon user",
-  }
+  package { "torquebox":
+    ensure => latest
+  } ->
 
   # Install an init script for the Razor torquebox install
   file { "/etc/init.d/razor-server":
@@ -35,7 +18,7 @@ class razor::torquebox {
   file { "/opt/razor-torquebox/jboss/standalone":
     ensure  => directory, owner => $user, group => $user,
     recurse => true, checksum => none,
-    require => Exec["install torquebox binary distribution to ${dest}"]
+    require => Package[torquebox],
   } ->
 
   service { "razor-server":
