1ac4d75bd19ebb9d654006872123cb554cad4242
fhats <fred.hatfull@gmail.com>
2013-12-09 23:36:45 -0800

    Get rid of tag validation cause apparently it causes more harm than good

--- lib/puppet/type/razor_policy.rb
+++ lib/puppet/type/razor_policy.rb
@@ -51,23 +51,19 @@ Puppet::Type.newtype(:razor_policy) do
   newproperty(:max_count) do
     desc "The maximum number of machines that should be imaged using this policy"
     munge do |value|
-      value.to_i
+      value.to_s
     end
   end
 
   newproperty(:rule_number) do
     desc "The precedence this policy should take when overlapping with other policies."
     munge do |value|
-      value.to_i
+      value.to_s
     end
   end
 
   newproperty(:tags, :array_matching => :all) do
     desc "An array of the tags that should be used for matching this policy."
-    validate do |value|
-      fail("tags must be an array") unless value.is_a?(Array)
-      fail("each entry in tags must be a hash") unless value.all? { |x| x.is_a?(Hash) }
-    end
   end
 
 end
